ed051b6e1e5b512a29583ed61d584ebb7c6f9e5f
MatthieuDep <matthieu.depret.md@gmail.com>
2026-01-29 18:53:03 +0100

    Ajout du state local sur le bouton like

--- src/components/RestaurantCard/RestaurantCard.jsx
+++ src/components/RestaurantCard/RestaurantCard.jsx
@@ -1,7 +1,16 @@
+"use client"
+
 import styles from "./RestaurantCard.module.css";
+import { useState } from "react";
 
 export default function RestaurantCard({ restaurant }) {
   const {name, location, image, isNew} = restaurant;
+
+  const [isLiked, setIsLiked] = useState(false);
+
+  const toggleLike = () => {
+    setIsLiked((prev) => !prev);
+  };
 
   return (
     <article className={styles.card}>
@@ -20,22 +29,36 @@ export default function RestaurantCard({ restaurant }) {
           <p className={styles.location}>{location}</p>
         </div>
 
-        <button className={styles.heartButton} aria-label="Ajouter aux favoris">
+        <button className={styles.heartButton} aria-label="Ajouter aux favoris" onClick={toggleLike}>
           <svg
-            className={styles.heartIcon}
+            className={`${styles.heartIcon} ${
+              isLiked ? styles.liked : ""
+            }`}
             width="24"
             height="24"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
+            <defs>
+              <linearGradient id="heartGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                <stop offset="0%" stopColor="#9356DC" />
+                <stop offset="100%" stopColor="#FF79DA" />
+              </linearGradient>
+            </defs>
+
             <path
-              d="M12.1 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3 9.24 3 10.91 3.81 12 5.08 13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5 22 12.28 18.6 15.36 13.35 20.04 L12.1 21.35Z"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
+              d="M12.1 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5
+                2 5.42 4.42 3 7.5 3
+                9.24 3 10.91 3.81 12 5.08
+                13.09 3.81 14.76 3 16.5 3
+                19.58 3 22 5.42 22 8.5
+                22 12.28 18.6 15.36 13.35 20.04
+                L12.1 21.35Z"
+              fill={isLiked ? "url(#heartGradient)" : "none"}
+              stroke={isLiked ? "none" : "currentColor"}
+              strokeWidth={isLiked ? 0 : 2}
             />
           </svg>
-
         </button>
       </div>
     </article>
